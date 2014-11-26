f5f0a395c44e520694e6fd1791541c1e06a2aca9
christoph-eb <christoph.ebbers@gmail.com>
2014-11-26 08:12:31 +0100

    *added header *added decimal places to five (temperature)

--- system_a/SinkE1.java
+++ system_a/SinkE1.java
@@ -28,74 +28,79 @@ import java.util.Calendar;
 
 
 public class SinkE1 extends SinkFilter {
-	boolean firstWrite = true;
-	String fileName = "OutputA.dat";
-	
-	/**
+    boolean firstWrite = true;
+    String fileName = "OutputA.dat";
+
+    /**
     * The method "processValueSet" is responsible for formatting and
     * and writting the data in the respective file.
     *
-    * @param data which will be written in "OutputA.dat" 
+    * @param data which will be written in "OutputA.dat"
     */
-	public void processValueSet(DataStruct data) {
-		
-		// Create the new output file for "OutputA".
+    public void processValueSet(DataStruct data) {
+
+        // Create the new output file for "OutputA".
         // If the file exists, the file will be deleted
         // and a new File will be created.
-		if(firstWrite){
-			File f = new File(fileName);
-			try {
-				if(f.exists())f.delete();
-				f.createNewFile();
-				firstWrite = false;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.print("Error while create output file.");
-			}
-		}
-		
-			String printString = "";
+        if (firstWrite) {
+            File f = new File(fileName);
+            try {
+                if (f.exists())f.delete();
+                f.createNewFile();
+                firstWrite = false;
+                String header = "Time: \t\t\t\t\t\t" + "Temperature (C): \t\t" + "Altitude (m):\n" +
+                                "___________________________________________________________________________";
+                writeData(header);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                System.out.print("Error while create output file.");
+            }
+        }
 
-			// Get the timestamp in the format yyyy MM dd::hh:mm:ss (e.g. 2014 01 14::07:04:11).
-			// The timestamp is written in the first column.
-			Calendar TimeStamp = Calendar.getInstance();
-			SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd::hh:mm:ss");
-			TimeStamp.setTimeInMillis(data.Time.longValue());
-			printString += TimeStampFormat.format(TimeStamp.getTime()) + "\t";
+        String printString = "";
 
-			// If the value of the temperature (in Celsius) isn't empty then,
-			// formats the temperature and add it to the output string.
-			if(data.Temp != null){
-				DecimalFormatSymbols tempDecimalFormatSymbols = new DecimalFormatSymbols();
-				tempDecimalFormatSymbols.setDecimalSeparator('.');
-				DecimalFormat TempdecimalFormat = new DecimalFormat("+#,000.000;-#", tempDecimalFormatSymbols);
-				printString += TempdecimalFormat.format(Double.longBitsToDouble(data.Temp.longValue()));
-			}
-			printString += "\t";
-			
-			// If the value of the altitude (in meters) isn't empty then,
-			// formats the altitude and add it to the output string.
-			if(data.Altitude != null){
-				DecimalFormatSymbols attitudeDecimalFormatSymbols = new DecimalFormatSymbols();
-				attitudeDecimalFormatSymbols.setDecimalSeparator('.');
-				DecimalFormat attitudeDecimalFormat = new DecimalFormat("000000.00000", attitudeDecimalFormatSymbols);
-				printString += attitudeDecimalFormat.format(Double.longBitsToDouble(data.Altitude.longValue()));
-			}
-			printString  += "\n";
-		
-		    // Writes the whole string in the "OutputB.dat".
-			// First column     --> Date and time information
-			// Second column    --> The temperature in Celsius
-			// Third column     --> The altitude in meters
-		try {
-		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
-		    out.println(printString);
-		    out.close();
-		} catch (IOException e) {
-		    //exception handling left as an exercise for the reader
-		}
-		
-		
-	}
-	
+        // Get the timestamp in the format yyyy MM dd:hh:mm:ss (e.g. 2014 01 14:07:04:11).
+        // The timestamp is written in the first column.
+        Calendar TimeStamp = Calendar.getInstance();
+        SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd:hh:mm:ss");
+        TimeStamp.setTimeInMillis(data.Time.longValue());
+        printString += TimeStampFormat.format(TimeStamp.getTime()) + "\t\t\t";
+
+        // If the value of the temperature (in Celsius) isn't empty then,
+        // formats the temperature and add it to the output string.
+        if (data.Temp != null) {
+            DecimalFormatSymbols tempDecimalFormatSymbols = new DecimalFormatSymbols();
+            tempDecimalFormatSymbols.setDecimalSeparator('.');
+            DecimalFormat TempdecimalFormat = new DecimalFormat("+#,000.00000;-#", tempDecimalFormatSymbols);
+            printString += TempdecimalFormat.format(Double.longBitsToDouble(data.Temp.longValue()));
+        }
+        printString += "\t\t\t\t";
+
+        // If the value of the altitude (in meters) isn't empty then,
+        // formats the altitude and add it to the output string.
+        if (data.Altitude != null) {
+            DecimalFormatSymbols attitudeDecimalFormatSymbols = new DecimalFormatSymbols();
+            attitudeDecimalFormatSymbols.setDecimalSeparator('.');
+            DecimalFormat attitudeDecimalFormat = new DecimalFormat("000000.00000", attitudeDecimalFormatSymbols);
+            printString += attitudeDecimalFormat.format(Double.longBitsToDouble(data.Altitude.longValue()));
+        }
+        writeData(printString);
+    }
+
+
+    // Writes the whole string in the "OutputB.dat".
+    // Header
+    // First column     --> Date and time information
+    // Second column    --> The temperature in Celsius
+    // Third column     --> The altitude in meters
+    public void writeData(String dataToWrite) {
+        try {
+            PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+            writer.println(dataToWrite);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error while writing in " + fileName + " --> " + e.getMessage());
+        }
+    }
+
 }
